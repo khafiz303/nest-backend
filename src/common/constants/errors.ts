92eafac2277cb76a60a6dae089c8349aee193228
Khafiz <sadirovhafiz06@gmail.com>
2024-08-26 21:03:43 +0500

    Create auth module , Create methods login and register , Cerate JWT guard and JWT strategy  , Create methods update user and delete user

---
+++ src/common/constants/errors.ts
@@ -0,0 +1,7 @@
+export const AppError = {
+    USER_EXIST : 'User with this  email exist ',
+    USER_NOT_EXIST : 'User with this  email not exist ',
+    WRONG_DATA : 'Wrong data'
+
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './modules/app/app.module';
 import {ConfigService} from '@nestjs/config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 
 
@@ -8,6 +10,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService)
   const port = configService.get('port')
+  app.useGlobalPipes(new ValidationPipe())
+
+
+  const config =new  DocumentBuilder()
+    .setTitle('Lesson API')
+    .setDescription('This api for lesson')
+    .setVersion('1.0')
+    .addTag('API')
+    .build()
+  const document = SwaggerModule.createDocument(app , config)
+  SwaggerModule.setup('api' , app , document)
   await app.listen(port);
 
   
