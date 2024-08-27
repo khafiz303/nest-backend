1c7d1da240b7ee6c48261f84c124ccf7c63911c7
Khafiz <sadirovhafiz06@gmail.com>
2024-08-27 16:51:54 +0500

    Bugfix in  application - Improved auth service , Improved token service

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -21,7 +21,7 @@ export class AuthController {
     @ApiResponse({status : 200 , type :AuthUserResponse  })
     @ApiTags('API')
     @Post('login')
-    login(@Body() dto : UserLoginDTO) : Promise<any>{
+    login(@Body() dto : UserLoginDTO) : Promise<AuthUserResponse>{
         return this.authService.loginUser(dto)
     }
 
--- src/modules/auth/response/index.ts
+++ src/modules/auth/response/index.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger"
 import { IsString } from "class-validator"
 
-export class AuthUserResponse{
+class UserResponse{
     @ApiProperty()
     @IsString()
     firstName : string 
@@ -17,10 +17,17 @@ export class AuthUserResponse{
     @ApiProperty()
      @IsString()
     password : string
+}
+
+export class AuthUserResponse{
+    @ApiProperty()
+    user : UserResponse
 
     @ApiProperty()
     @IsString()
     token: string
+
+
 
 
   
